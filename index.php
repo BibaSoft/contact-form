a4986d0c0c0d0c1e323f4598a4de3cdce683f751
Ha Biba <w.habouba@gmail.com>
2018-07-20 18:22:53 +0000

    creating home page

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+     <title>Contact Form </title>
+     <meta charset="utf-8" lang="en">
+     <link rel="stylesheet" href="css/bootstrap.min.css"/>
+     <link rel="stylesheet" href="css/font-awesome.min.css"/>
+     <link rel="stylesheet" href="css/contact.css"/>
+    </head>
+
+
+<body>
+<script src="js/jquery-1.12.4.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+
+
+
+
+
+</html>
