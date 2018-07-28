c21cd49a8c5fef3ae6c16b47c3d7ef50764b32b7
Ha Biba <w.habouba@gmail.com>
2018-07-28 00:46:39 +0000

    adding form

--- index.php
+++ index.php
@@ -3,15 +3,33 @@
     <head>
      <title>Contact Form </title>
      <meta charset="utf-8" lang="en">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1">
      <link rel="stylesheet" href="css/bootstrap.min.css"/>
      <link rel="stylesheet" href="css/font-awesome.min.css"/>
      <link rel="stylesheet" href="css/contact.css"/>
+         <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
     </head>
 
 
 <body>
 <script src="js/jquery-1.12.4.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
+    <div class="container">
+        <form>
+        <input class="form-control" type="text" name="username" placeholder=""/>
+         <input class="form-control" type="email" name="email"/>
+             <input class="form-control" type="password" name="pwd"/>
+             <input class="form-control" type="text" name="username"/>
+            <textarea class="form-control"></textarea>
+            <input class="btn btn-success btn-block" type"submit" value="Send"/>
+        </form>        
+        <div/>
 </body>
 
 
